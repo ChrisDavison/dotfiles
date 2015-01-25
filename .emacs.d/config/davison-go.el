64427ba9096d0b340ed3094849ce6483b4825f99
Christopher Davison <c.jr.davison@gmail.com>
2015-01-25 14:54:43 +0000

    Simplified emacs config into literate org file

--- .emacs.d/config/davison-go.el
+++
@@ -1,6 +0,0 @@
-(require-package 'go-mode)
-(require-package 'go-autocomplete)
-
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-(provide 'davison-go)
--- .emacs.d/config/davison-keychord.el
+++
@@ -1,6 +0,0 @@
-(require-package 'key-chord)
-(key-chord-mode 1)
-(key-chord-define-global "jk" 'evil-normal-state)
-(key-chord-define-global "gc" 'comment-or-uncomment-region)
-
-(provide 'davison-keychord)
