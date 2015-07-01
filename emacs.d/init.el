658bc20db97ac4fd211d0ee1294482b777fb3883
Christopher Davison <c.jr.davison@gmail.com>
2015-07-01 22:27:11 +0100

    Fully bootstrapped with use-package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,11 +5,15 @@
 (package-initialize t)
 (setq package-enable-at-startup nil)
 
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
 ;; Try the 'use package' macro
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; From here on, the org config will use the use-package macro for installing and configuring packages
 (require 'use-package)
 
 ;; Finally, load my Org-mode (literate programming) config file
