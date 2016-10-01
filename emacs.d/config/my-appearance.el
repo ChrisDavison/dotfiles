563efbf2897ed863aa46da23c754dc4d3d423e82
Christopher Davison <c.jr.davison@gmail.com>
2016-10-01 17:03:09 +0100

    Multi-file emacs config

---
+++ emacs.d/config/my-appearance.el
@@ -0,0 +1,104 @@
+;;; my-appearance.el --- Appearance of my emacs config
+
+;;; Commentary:
+
+;;; Code:
+
+;; FONT
+
+(when (member "Input Mono" (font-family-list))
+  (set-default-font "Input Mono 15"))
+(setq line-spacing 0.2)
+
+;; colour theme
+
+;; Disable themes before loading a new theme
+(defadvice load-theme (before theme-dont-propagate activate)
+  (mapc #'disable-theme custom-enabled-themes))
+
+;; I always end up going back to solarized, but have been using 'github' within
+;; VIM, so should look into alternatives.  Also added functions to quickly jump
+;; between light and dark themes.
+
+(use-package solarized-theme :ensure t)
+(use-package molokai-theme :ensure t)
+(use-package seoul256-theme :ensure t)
+(use-package color-theme-sanityinc-tomorrow :ensure t)
+(use-package tao-theme :ensure t)
+
+(setq theme_dark nil)
+(setq dark_theme 'solarized-dark)
+(setq light_theme 'solarized-light)
+
+(defun toggle-dark-theme ()
+  "Toggle theme between light and dark"
+  (interactive)
+  (if theme_dark
+      (progn
+        (load-theme dark_theme t)
+        (setq theme_dark nil))
+    (progn
+      (load-theme light_theme t)
+      (setq theme_dark t))))
+(toggle-dark-theme) ;; Load theme, based on theme_dark var
+
+;; *maybe* suspend frame
+;; This is useful if running a mac, so that C-z wont hide it.  Considered
+;; 'appearance' as I want to see windows.
+(defun maybe-suspend-frame ()
+  (interactive)
+  (unless (and *is-a-mac* window-system)
+    (suspend-frame)))
+(global-set-key (kbd "C-z") 'maybe-suspend-frame)
+
+;; suppress gui features
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message t)
+
+;; Hide tool bar,  scroll bar and borders
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
+
+(let ((no-border '(internal-border-width . 0)))
+  (add-to-list 'default-frame-alist no-border)
+  (add-to-list 'initial-frame-alist no-border))
+
+;; highlight current line and no blinking cursor
+(global-hl-line-mode 1)
+(blink-cursor-mode 0)
+(setq linum-format "%d ")
+
+;; alias for yes-or-no
+;; This is again a quality of life thing, to allow you to hit yes or no by using
+;; y or n.
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; faster keystroke echoing
+(setq echo-keystrokes 0.1)
+
+;; tabs to spaces, and tab-related things
+(setq tab-stop-list (number-sequence 2 200 2))
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+
+;; show line and column number in status bar
+(line-number-mode 1)
+(column-number-mode 1)
+
+;; stop scrolling jumping multiple lines
+(setq scroll-step           1
+      scroll-conservatively 10000)
+
+;; scratch buffer - initial message
+(setq-default initial-scratch-message ";; Scratch pad\n\n")
+
+;; font-locking
+;; Replace any occurrence of 'lambda' with the actual symbol.
+(global-prettify-symbols-mode +1)
+
+(provide 'my-appearance)
+;;; my-appearance.el ends here
