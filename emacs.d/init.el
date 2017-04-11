bb10e1b61133b1094c912ccb46bb4e9f3367d67e
Chris Davison <c.jr.davison@gmail.com>
2017-04-11 16:05:51 +0100

    Fixing merge

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -50,3 +50,17 @@
 (provide 'init)
 
 ;;; init ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ace-isearch virtualenvwrapper elpy magit exec-path-from-shell guide-key f s counsel ivy fold-dwim-org fold-dwim ibuffer-vc switch-window smartscan anzu dired+ cargo company flycheck-rust flymake-rust rust-mode go-mode rainbow-mode js-comint nodejs-repl coffee-mode json-mode js2-mode scss-mode sass-mode haml-mode emmet-mode lua-mode csv-nav pandoc-mode markdown-mode flycheck-clojure aggressive-indent expand-region evil-smartparens smartparens key-chord evil-leader evil-surround evil tao-theme color-theme-sanityinc-tomorrow seoul256-theme molokai-theme solarized-theme fullframe use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
