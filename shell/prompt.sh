1df2adbd50833f5eb110e287e1a23d47ef0e5905
Chris Davison <c.jr.davison@gmail.com>
2018-03-05 09:34:26 +0000

    sh: git branch in right prompt

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -20,3 +20,33 @@ local return_status="%(?:%{%F{blue}%}$prompt_string:%{%F{red}%}$prompt_string)"
 
 # PROMPT="${host_name} ${path_string} ${return_status} %{$reset_color%}"
 PROMPT="${path_string} ${return_status} %{%F{reset}%}"
+
+
+# ===== Right prompt - git repo and branch =====
+# Based on:
+# --- http://stackoverflow.com/a/1128583
+# --- http://stackoverflow.com/a/1128721
+
+_git_repo_name() {
+    gittopdir=$(git rev-parse --git-dir 2> /dev/null)
+    if [[ "foo$gittopdir" == "foo.git" ]]; then
+        echo `basename $(pwd)`
+    elif [[ "foo$gittopdir" != "foo" ]]; then
+        echo `dirname $gittopdir | xargs basename`
+    fi
+}
+
+_git_branch_name() {
+    git branch 2>/dev/null | awk '/^\*/ { print $2 }'
+}
+
+_git_is_dirty() {
+   git diff --quiet 2> /dev/null || echo '*'
+}
+
+autoload -U colors
+colors
+
+setopt prompt_subst
+
+RPROMPT='$(_git_repo_name) $(_git_branch_name) $(_git_is_dirty)'
