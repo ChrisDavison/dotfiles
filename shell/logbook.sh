a5937f8382d77a52594c4b278a1f830d6d537bfb
Chris Davison <c.jr.davison@gmail.com>
2018-04-17 15:15:52 +0100

    Function to return last weeks logbook

--- shell/logbook.sh
+++ shell/logbook.sh
@@ -1,10 +1,19 @@
 export LOGBOOK_DIR="$HOME/src/github.com/chrisdavison/logbook/"
 
-function this_weeks_logbook(){
+this_weeks_logbook(){
     a=$(date +"%Y/")
     a+=$(date -v -monday +"%Y%m%d")
     a+="--"
     a+=$(date -v +sunday +"%Y%m%d")
+    a+=".md"
+    echo $LOGBOOK_DIR$a
+}
+
+last_weeks_logbook(){
+    a=$(date +"%Y/")
+    a+=$(date -v -monday -v-7d +"%Y%m%d")
+    a+="--"
+    a+=$(date -v +sunday -v-7d +"%Y%m%d")
     a+=".md"
     echo $LOGBOOK_DIR$a
 }
