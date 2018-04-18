3537772a200ff64fda91fd50c2b948034e4d4e18
Chris Davison <c.jr.davison@gmail.com>
2018-04-18 08:50:06 +0100

    Remove _%H%M from notetaking

--- shell/notetaking.sh
+++ shell/notetaking.sh
@@ -2,17 +2,9 @@ export TODOFILE="$HOME/Dropbox/n/notes/todo.txt"
 export EXTERNAL_BRAIN="$HOME/Dropbox/n/notes/capture.txt"
 noteadd(){
     if [ -e "$EXTERNAL_BRAIN" ]; then
-        echo "- $(date +'`%Y%m%d_%H%M`') $1" >> "$EXTERNAL_BRAIN"
+        echo "$(date +'%Y%m%d') $1" >> "$EXTERNAL_BRAIN"
     else
-        echo "- $(date +'`%Y%m%d_%H%M`') $1" > "$EXTERNAL_BRAIN"
-    fi
-}
-
-todoadd(){
-    if [ -e "$TODOFILE" ]; then
-        echo "$(date +'`%Y%m%d_%H%M`') $1" >> "$TODOFILE"
-    else
-        echo "$(date +'`%Y%m%d_%H%M`') $1" > "$TODOFILE"
+        echo "$(date +'%Y%m%d') $1" > "$EXTERNAL_BRAIN"
     fi
 }
 
@@ -33,7 +25,7 @@ notefile(){
 }
 
 alias n="noteadd"
-alias note="noteadd"
+alias na="noteadd"
 alias nv="noteview"
 alias nc="noteclear"
 alias t="todo.sh"
