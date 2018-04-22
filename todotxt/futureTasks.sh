08d6952408d81ff0d5080fb42e1c3f26a8fbf32e
Chris Davison <c.jr.davison@gmail.com>
2018-04-22 22:30:37 +0100

    Simplify shell version of futuretasks

--- todotxt/futureTasks.sh
+++ todotxt/futureTasks.sh
@@ -1,13 +1,10 @@
+#!/usr/bin/env sh
 now=$(date +%s)
 while read line; do
     scheduleFromLine=$(echo "$line" | egrep -o 't:20[0-9]{2}-[0-9]{2}-[0-9]{2}')
-    dateFromLine=$(echo "$scheduleFromLine" | cut -c3-)
-    if [ "$dateFromLine" = "" ]; then
+    date=$(echo "$scheduleFromLine" | cut -c3-)
+    dateAsEpochSeconds=$(date -d "$date" +%s)
+    if [ "$date" = "" ] || [ "$now" -gt "$dateAsEpochSeconds" ]; then
         echo "$line"
-    else
-        dateAsEpochSeconds=$(date -d "$dateFromLine" +%s)
-        if [ "$now" -gt "$dateAsEpochSeconds" ]; then
-            echo "$line"
-        fi
     fi
 done
