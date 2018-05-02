984c8374d868ebd919871d5146775d91d2e9f6e2
Chris Davison <c.jr.davison@gmail.com>
2018-05-02 14:50:37 +0100

    Refactor headtail with some subroutines and usage

--- bin/headtail.pl
+++ bin/headtail.pl
@@ -5,13 +5,14 @@ use warnings;
 use 5.26.0;
 use Scalar::Util qw/looks_like_number/;
 
-sub main{
+sub main {
+    # Read in arguments
     my $nargs = @ARGV;
-    die ("Not enough args") unless ($nargs >= 1);
+    usage() unless ($#ARGV >= 0);
     my $file = $ARGV[0];
-    open( my $fh, '<', $file ) or die "Can't read file '$file' [$!]\n";
-    my @rows = <$fh>;
-    my $rownum0 = $ARGV[1] // 0;
+    my $rownum0 = $ARGV[1] // 0;  # Use first row (idx 0) if no row is given
+
+    my @rows = lines_from_file($file); 
     $rownum0 = looks_like_number($rownum0) ? $rownum0 : 0;
     my $row0 = $rows[$rownum0] or die("Couldn't get row $rownum0 from $file");
     my $rowN = $rows[$#rows] or die("Couldn't get row $#rows from $file");
@@ -20,6 +21,21 @@ sub main{
     my $lenLongestStr = length ($#rows . "");
     printf "%$lenLongestStr"."d: %s\n", $rownum0, $row0;
     printf "%$lenLongestStr"."d: %s\n", $#rows, $rowN;
+}
+
+sub lines_from_file {
+    my $file = shift;
+    open( my $fh, '<', $file ) or die "Can't read file '$file' [$!]\n";
+    my @rows = <$fh>;
+    close $fh;
+    return @rows;
+}
+
+sub usage {
+    say "usage: $0 FILE [N]";
+    say "    Print the Nth and last row from FILE";
+    say "    displaying line number and file contents";
+    exit 1;
 }
 
 main() or die("main failed");
