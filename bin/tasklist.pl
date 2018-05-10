15c87264953bdb5a8863f3724019274baa2bdba1
Chris Davison <c.jr.davison@gmail.com>
2018-05-10 15:38:20 +0100

    Refactor task search into subroutine

--- bin/tasklist.pl
+++ bin/tasklist.pl
@@ -16,20 +16,31 @@ sub main {
 
     my $filter = shift @ARGV;
 
-    for (@taskfiles) {
-        my $fn = $_;
-        $fn =~ s/~/$ENV{HOME}/g;
-        open( my $fh_task, '<', $fn );
-        my @lines = <$fh_task>;
-        close $fh_task;
-        my @tasks = grep { /\{#[a-z]+-[a-z]+\}/ } @lines;
+    for my $taskfile (@taskfiles) {
+        chomp $taskfile;
+        my @tasks = search_for_tasks($taskfile);
         if( $filter ) {
             @tasks = grep { /$filter/i } @tasks
         }
+        my $prefix = "$taskfile: ";
+        $prefix = "" if( $#taskfiles == 0 );
         for( @tasks ) {
             $_ =~ s/^#+ //g;
             $_ =~ s/^\*\*(.*)\*\*/$1 :: /g;
-            print $_;
+            print "$prefix$_";
         };
     }
+}
+
+sub search_for_tasks {
+    my $fn = shift @_;
+    # Replace '~' with actual home directory
+    $fn =~ s/~/$ENV{HOME}/g;
+    # Read all lines
+    open( my $fh_task, '<', $fn );
+    my @lines = <$fh_task>;
+    close $fh_task;
+    # Filter only those matching {#WORD-WORD}
+    my @tasks = grep { /\{#[a-z]+-[a-z]+\}/ } @lines;
+    return @tasks;
 }
