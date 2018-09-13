9c06dcabb10f7491e3914b840b0ef08b20fcc602
Christopher Davison <c.jr.davison@gmail.com>
2018-09-13 12:09:05 +0100

    Only show git repos with changes

--- fish/functions/repostat.fish
+++ fish/functions/repostat.fish
@@ -1,10 +1,16 @@
 function repostat
 	for repo in ~/devel/*
         pushd $repo
-        set_color --bold magenta; echo -n "Status of" 
-        set_color yellow; echo " $repo"
-        set_color normal; git status -s -b
-        echo
+        if [ (git status -s | wc -l) -gt 0 ]
+            set_color --bold magenta; echo -n "Status of" 
+            set -l reposhort (basename $repo)
+            set -l branch (git rev-parse --abbrev-ref HEAD)
+            set_color yellow; echo -n " $reposhort"
+            set_color magenta; echo -n " on"
+            set_color yellow; echo " $branch"
+            set_color normal; git status -s
+            echo
+        end
         popd
     end
 end
