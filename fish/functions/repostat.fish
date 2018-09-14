c3360d246e097a7cfa6a7cbe01b364ebe2e623ab
Christopher Davison <c.jr.davison@gmail.com>
2018-09-14 13:43:01 +0100

    Improve perf of repostat

--- fish/functions/repostat.fish
+++ fish/functions/repostat.fish
@@ -2,19 +2,18 @@ function repostat
 	for repo in ~/devel/*
         pushd $repo
         set -g show 0
-        if [ (git status -s | wc -l) -gt 0 ]
-            set show 1
-        else if [ (git status -s -b | grep -E -e "ahead|behind" | wc -l) -gt 0 ]
-            set show 1
-        end
-        if [ $show -eq 1 ]
+        git status -s -b > ~/.stat
+        # set -g gstatus (git status -s -b)
+        set -g lineschanged (cat ~/.stat | wc -l)
+        set -g linesbehind (cat ~/.stat | grep -E -e "ahead|behind" | wc -l)
+        if [ $lineschanged -gt 1 ]; or [ $linesbehind -gt 0 ]
             set_color --bold magenta; echo -n "Status of" 
             set -l reposhort (basename $repo)
             set -l branch (git rev-parse --abbrev-ref HEAD)
             set_color yellow; echo -n " $reposhort"
             set_color magenta; echo -n " on"
             set_color yellow; echo " $branch"
-            set_color normal; git status -s -b
+            set_color normal; cat ~/.stat
             echo 
         end
         popd
