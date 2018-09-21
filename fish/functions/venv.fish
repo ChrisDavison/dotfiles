92993eee37c82ec4be04a87be804c10da17aecd7
Christopher Davison <c.jr.davison@gmail.com>
2018-09-21 12:58:38 +0100

    Use `fd` if possible in fish VENV function

--- fish/functions/venv.fish
+++ fish/functions/venv.fish
@@ -1,8 +1,15 @@
 function venv --description "Choose a python virtualenv"
+    set -g selected ""
     if [ -d ~/.envs ]
-        set -l selected (find ~/.envs -type f -regex ".*\/activate.fish" | fzf -q "$argv[1]")
+        if [ -x (which fd) ]
+            set -g selected (fd "activate.fish" ~/.envs | fzf -q "$argv[1]")
+        else
+            set -g selected (find ~/.envs -name "activate.fish" | fzf -q "$argv[2]")
+        end
         if [ -n "$selected" ]
             source "$selected"
+        else
+            echo "Selected was empty"
         end
     else
         echo "Couldn't find virtualenvs"
