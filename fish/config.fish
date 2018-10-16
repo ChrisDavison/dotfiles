db3ab97894a59b8fed5d66d29779c6869b4ca956
Christopher Davison <c.jr.davison@gmail.com>
2018-10-16 10:00:58 +0100

    Only add to /Users/davison/bin /Users/davison/.cargo/bin /Users/davison/Library/Python/3.7/bin/ /Users/davison/.multirust/toolchains/stable-x86_64-apple-darwin/bin /Users/davison/bin /usr/local/lib/node_modules /Users/davison/.vim/bundle/fzf/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /usr/local/share/dotnet /Users/davison/Library/Haskell/bin /usr/local/go/bin /Library/TeX/texbin if dir exists

--- fish/config.fish
+++ fish/config.fish
@@ -9,13 +9,13 @@ set --export CAPTUREFILE $HOME/Dropbox/.capture
 set --export LOGBOOK_DIR $HOME/src/github.com/chrisdavison/logbook
 set --export RESEARCHFIGURES $HOME/Dropbox/work/figures
 
-set PATH $HOME/.vim/bundle/fzf/bin $PATH
-set PATH /usr/local/lib/node_modules $PATH
-set PATH $GOBIN $PATH
-set PATH $HOME/.multirust/toolchains/stable-x86_64-apple-darwin/bin $PATH
-set PATH $HOME/Library/Python/3.7/bin/ $PATH
-set PATH $HOME/.cargo/bin $PATH
-set PATH $HOME/bin $PATH
+test -d $HOME/.vim/bundle/fzf/bin; and set PATH $HOME/.vim/bundle/fzf/bin $PATH
+test -d /usr/local/lib/node_modules; and set PATH /usr/local/lib/node_modules $PATH
+test -d $GOBIN; and set PATH $GOBIN $PATH
+test -d $HOME/.multirust/toolchains/stable-x86_64-apple-darwin/bin; and set PATH $HOME/.multirust/toolchains/stable-x86_64-apple-darwin/bin $PATH
+test -d $HOME/Library/Python/3.7/bin/; and set PATH $HOME/Library/Python/3.7/bin/ $PATH
+test -d $HOME/.cargo/bin; and set PATH $HOME/.cargo/bin $PATH
+test -d $HOME/bin; and set PATH $HOME/bin $PATH
 
 if [ -f /usr/local/share/autojump/autojump.fish ]
     source /usr/local/share/autojump/autojump.fish
