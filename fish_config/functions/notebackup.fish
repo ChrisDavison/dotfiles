d90c7b4a74a3fa2afaf534af6026e67d7a03cf83
Chris Davison <c.jr.davison@gmail.com>
2019-02-14 13:48:32 +0000

    Remove verbose flag from rsync in notebackup

--- fish_config/functions/notebackup.fish
+++ fish_config/functions/notebackup.fish
@@ -1,14 +1,19 @@
 function notebackup
+    set -l verbose 0
+    if [ (count $argv) -gt 0 ] && [ $argv[1] = "-v" ]
+        set -l verbose 1
+    end
     if [ ! -d $NOTESDIR ]
         echo "NOTESDIR and NOTESBACKUPDIR must both be defined"
         echo "NOTESDIR: "$NOTESDIR
         echo "NOTESBACKUPDIR: "$NOTESBACKUPDIR
     end
     set dt (date +"%Y%m%dT%H%M")
-    echo $dt": "$NOTESDIR" Backup"
+    [ $verbose -eq 1 ]; and echo $dt": "$NOTESDIR" Backup"
+    [ $verbose -eq 1 ]; and echo "Removing all files"
     rm -rf $NOTESBACKUPDIR/*
     if type -q rsync
-        rsync -avz $NOTESDIR/* $NOTESBACKUPDIR
+        rsync -az $NOTESDIR/* $NOTESBACKUPDIR
     else
         cp -r $NOTESDIR/* $NOTESBACKUPDIR/
     end
@@ -17,4 +22,5 @@ function notebackup
     git commit -m "Backup "$dt
     git push
     git archive -o $HOME/notes-backup--$dt.zip @
+    [ $verbose -eq 1 ]; and echo "Backed up. Zip: "$HOME/notes-backup--$dt.zip
 end
