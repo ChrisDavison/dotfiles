5b5539b92285f957d7d4e40f1ddff726682f1154
Chris Davison <c.jr.davison@gmail.com>
2019-02-14 14:08:01 +0000

    Function to interactively pick asmr vid

---
+++ fish_config/functions/asmri.fish
@@ -0,0 +1,15 @@
+function asmri
+	if [ (count $argv) -gt 0 ]
+set matching (asmrfind | fzf -q $argv)
+else
+set matching (asmrfind | fzf)
+end
+if [ -z $matching ]
+echo "No match"
+return 1
+end
+set name (echo $matching | cut -d';' -f1)
+set vidid (echo $matching | cut -d';' -f2)
+echo "Watching: $name"
+echo "https://youtube.com/watch?v="$vidid | OpenInBrowser 
+end
