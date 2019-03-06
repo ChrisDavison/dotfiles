e566892045e8cc36229b26ec05ab2e9efdd4707c
Chris Davison <c.jr.davison@gmail.com>
2019-03-06 19:12:38 +0100

    Don't error if no repo has status change

--- repobstat.sh
+++ repobstat.sh
@@ -3,16 +3,21 @@ set -o errexit  # Exit early on error
 set -o pipefail # Exit when a pipe fails
 set -o nounset  # Exit when trying to use undeclared variables
 
+outname="$HOME/.repos_status"
 repoloc="${HOME}/code"
 for dir in ${repoloc}/*; do
     pushd $dir 2>&1 > /dev/null
     tidy=$(basename $(pwd))
     gstat=$(git branchstat)
     if [ -n "$gstat" ]; then
-        echo "$tidy:$gstat" >> ~/.repostat_all
+        echo "$tidy:$gstat" >> ${outname}
     fi
     popd 2>&1 > /dev/null
 done
 
-cat ~/.repostat_all | column -s':' -t
-rm ~/.repostat_all
+if [ -f ${outname} ]; then
+    cat ${outname} | column -s':' -t
+    rm ${outname}
+else
+    echo "No repo has changes"
+fi
