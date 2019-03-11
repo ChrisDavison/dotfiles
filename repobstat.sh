dec866aae5cc1484d3c9836073a0d27ba76ac021
Chris Davison <c.jr.davison@gmail.com>
2019-03-11 08:35:57 +0000

    Extract repobstat function

--- repobstat.sh
+++ repobstat.sh
@@ -3,17 +3,22 @@ set -o errexit  # Exit early on error
 set -o pipefail # Exit when a pipe fails
 set -o nounset  # Exit when trying to use undeclared variables
 
-outname="$HOME/.repos_status"
-repoloc="${HOME}/code"
-for dir in ${repoloc}/*; do
-    pushd $dir 2>&1 > /dev/null
-    tidy=$(basename $(pwd))
-    gstat=$(git branchstat)
-    if [ -n "$gstat" ]; then
-        echo "$tidy:$gstat" >> ${outname}
-    fi
-    popd 2>&1 > /dev/null
-done
+stat_in(){
+    repoloc="$1"
+    outname="$HOME/.repos_status"
+    for dir in ${repoloc}/*; do
+        pushd $dir 2>&1 > /dev/null
+        tidy=$(basename $(pwd))
+        gstat=$(git branchstat)
+        if [ -n "$gstat" ]; then
+            echo "$tidy:$gstat" >> ${outname}
+        fi
+        popd 2>&1 > /dev/null
+    done
+}
+
+stat_in "${HOME}/src/github.com/chrisdavison"
+stat_in "${HOME}/src/github.com/cidcom"
 
 if [ -f ${outname} ]; then
     cat ${outname} | column -s':' -t
