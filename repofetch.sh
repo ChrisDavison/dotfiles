5270abe43cee16a7fcb1b2d66df25f1c955eff9a
Chris Davison <c.jr.davison@gmail.com>
2019-03-25 11:14:16 +0000

    Use updated CODEDIR in repo utils

--- repofetch.sh
+++ repofetch.sh
@@ -3,10 +3,5 @@ set -o errexit  # Exit early on error
 set -o pipefail # Exit when a pipe fails
 set -o nounset  # Exit when trying to use undeclared variables
 
-fetch_in() {
-    echo "Fetching repos in $1";
-    parallel 'cd {} && git fetch -q --all' ::: "$1"/*
-}
-
-fetch_in "${HOME}/src/github.com/chrisdavison"
-fetch_in "${HOME}/src/github.com/cidcom"
+echo "Fetching repos in $CODEDIR";
+parallel 'cd {} && git fetch -q --all' ::: $CODEDIR/*
--- reposstat.sh
+++ reposstat.sh
@@ -3,9 +3,6 @@ set -o errexit  # Exit early on error
 set -o pipefail # Exit when a pipe fails
 set -o nounset  # Exit when trying to use undeclared variables
 
-loc1="${HOME}/src/github.com/chrisdavison"
-loc2="${HOME}/src/github.com/cidcom"
-
-for dir in ${loc1}/* ${loc2}/*; do
+for dir in $CODEDIR/*; do
     (cd "$dir" && git sstat)
 done
