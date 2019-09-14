69b73a0ba54ef0b9e0974a54c077baad77ae9872
Chris Davison <c.jr.davison@gmail.com>
2019-09-14 14:33:34 +0100

    Refreshing i3 automatically manages multi displays

--- polybar/polybar-launch.sh
+++ polybar/polybar-launch.sh
@@ -7,4 +7,12 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 MONITOR=LVDS1 polybar example &
-MONITOR=HDMI1 polybar example &
+
+hdmi1_available=$(xrandr -q | rg "HDMI1 disconnected" | wc -l)
+if [ $hdmi1_available -eq 1 ]; then
+    echo "HDMI1 not available"
+    xrandr --output HDMI1 --off
+else
+    xrandr --output HDMI1 --above LVDS1 --auto --noprimary
+    MONITOR=HDMI1 polybar example &
+fi
