4a5cdbbee376fdd622fb06712c25966ab9124c0e
Chris Davison <c.jr.davison@gmail.com>
2019-09-14 11:04:25 +0100

    Make polybar attach to both displays

--- polybar/polybar-launch.sh
+++ polybar/polybar-launch.sh
@@ -6,4 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar example &
+MONITOR=LVDS1 polybar example &
+MONITOR=HDMI1 polybar example &
