4b8da598e5ba34c3ff24794c9375b575c1014c11
Chris Davison <c.jr.davison@gmail.com>
2019-10-02 15:26:56 +0100

    Update bookmarks

--- .bin/dmenu_bookmarks.sh
+++ .bin/dmenu_bookmarks.sh
@@ -2,9 +2,10 @@
 
 site=$(cat ~/.bookmarks | cut -d';' -f1 | dmenu -i -l 10 -p "Site: " -fn "Ubuntu Mono-14")
 
-url=$(grep "$site" ~/.bookmarks | cut -d';' -f2)
-
-if [ ! -z "$url" ]; then
+if [ ! -z "$site" ]; then
+    url=$(grep "$site" ~/.bookmarks | cut -d';' -f2)
     xdg-open "$url"
 fi
+
+
 
