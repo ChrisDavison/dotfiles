c69e4723a9f9485c924dd9c4d9a4a9a09dc26ab5
Chris Davison <c.jr.davison@gmail.com>
2019-10-10 14:08:21 +0100

    Quote output filename in dmenu scrot

--- .bin/dmenu_screenshot.sh
+++ .bin/dmenu_screenshot.sh
@@ -10,8 +10,8 @@ opt=$(dmenu -l 10 -fn "Ubuntu Mono-18" -p "Screenshot mode: " <<< $menu | cut -d
 
 output_fn="$HOME/Dropbox/Camera Uploads/screenshots/%Y%m%d-%H%M%S.png" 
 case "$opt" in
-    focused) scrot -u $args $output_fn ;;
-    all) scrot -m $args $output_fn ;;
-    select) scrot -s $args $output_fn ;;
+    focused) scrot -u $args "$output_fn" ;;
+    all) scrot -m $args "$output_fn" ;;
+    select) scrot -s $args "$output_fn" ;;
     *) ;;
 esac
