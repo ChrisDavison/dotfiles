0b255a13e7b98ca178e80f7ed13a281a8ee4b19e
Chris Davison <c.jr.davison@gmail.com>
2019-10-14 10:09:59 +0100

    Add logbook to rofi projects

--- .bin/rofi_vscode.sh
+++ .bin/rofi_vscode.sh
@@ -6,6 +6,7 @@ if [ -z "$@" ]; then
         find $CODEDIR -mindepth 1 -maxdepth 1 -type d -exec realpath --relative-to=$HOME {} \;
     }
     echo $(realpath --relative-to=$HOME "$HOME/Dropbox/notes/")
+    echo $(realpath --relative-to=$HOME "$HOME/Dropbox/notes/logbook/")
     echo $(realpath --relative-to=$HOME "$HOME/Dropbox/thesis/analysis")
     gen_directories
 else
