2bb51c5593bf2a669fcacedce2f289704a971d89
Chris Davison <c.jr.davison@gmail.com>
2019-11-07 11:11:52 +0000

    Change scrot output dir

--- .bin/dmenu_screenshot.sh
+++ .bin/dmenu_screenshot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 menu="select rectangle
-focused window 
+focused window
 all displays
 "
 
 args="--quality 100"
 opt=$(dmenu -l 10 -fn "Ubuntu Mono-18" -p "Screenshot mode: " <<< $menu | cut -d' ' -f1)
 
-output_fn="$HOME/Dropbox/Camera Uploads/screenshots/%Y%m%d-%H%M%S.png" 
+output_fn="$HOME/Dropbox/Camera Uploads/scrot--%Y%m%d-%H%M%S.png"
 case "$opt" in
     focused) scrot -u $args "$output_fn" ;;
     all) scrot -m $args "$output_fn" ;;
