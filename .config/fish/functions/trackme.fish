7a79ce493b2b81839cdf04d4107d897d731494e6
Chris Davison <c.jr.davison@gmail.com>
2019-12-01 12:38:36 +0000

    Add more of a template to trackme func

--- .config/fish/functions/trackme.fish
+++ .config/fish/functions/trackme.fish
@@ -2,5 +2,7 @@ function trackme
     set title (string replace -a ' ' '-' "$argv")
     set fn (date +"$HOME/Dropbox/self-tracking/%Y%m%d-$title.txt")
     echo "title: $argv" > $fn
+    echo "value: " > $fn
+    echo "tags: " > $fn
 	nvim $fn
 end
