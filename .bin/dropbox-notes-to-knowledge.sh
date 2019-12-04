0a96089bd50fdf1066c531e6ca724eb7f2391d59
Chris Davison <c.jr.davison@gmail.com>
2019-12-04 22:02:57 +0000

    Use .txt when backing up dropbox notes

--- .bin/dropbox-notes-to-knowledge.sh
+++ .bin/dropbox-notes-to-knowledge.sh
@@ -6,15 +6,9 @@ notes_source="$HOME/Dropbox/notes/"
 notes_dest="$HOME/src/github.com/ChrisDavison/knowledge/"
 
 # sync everything from source (dropbox notes) to dest (github 'knowledge' repo)
-rsync -r $notes_source $notes_dest
+rsync -r --exclude "logbook/" $notes_source $notes_dest
 
 # change to the git repo and commit everything, with timestamp as commit message
-cd $notes_dest
-for file in *.txt **/*.txt
-do
-    mv "$file" "${file%.txt}.md"
-done
-[ -d "logbook" ] && rm -rf logbook/
 git add --all
 git commit -m "backup $(date +'%Y%m%d %H')"
 git push
