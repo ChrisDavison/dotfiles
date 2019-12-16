d9d734a7b47cb932314b4651de3df7e881a31e11
Chris Davison <c.jr.davison@gmail.com>
2019-12-16 15:19:30 +0000

    Add pylab flag to dipy

--- .config/fish/functions/dipy.fish
+++ .config/fish/functions/dipy.fish
@@ -1,3 +1,3 @@
 function dipy
-	docker run -it --rm -v (pwd):/home/jovyan/(basename (pwd)) -w /home/jovyan/(basename (pwd)) jupyter/tensorflow-notebook ipython
+    docker run -it --rm -v (pwd):/home/jovyan/(basename (pwd)) -w /home/jovyan/(basename (pwd)) jupyter/tensorflow-notebook ipython --pylab --no-banner
 end
