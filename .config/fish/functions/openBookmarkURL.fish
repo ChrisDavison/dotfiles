783769b2b289c822d06e139e081f011664560560
Chris Davison <c.jr.davison@gmail.com>
2019-12-17 17:15:27 +0000

    Make openBookmarkURL helper work on WSL

--- .config/fish/functions/openBookmarkURL.fish
+++ .config/fish/functions/openBookmarkURL.fish
@@ -1,5 +1,10 @@
 function openBookmarkURL
 	set bm $argv[1]
     set url (cat $bm | grep url | cut -d' ' -f2-)
-    xdg-open $url
+    uname -a | rg -q Microsoft
+    if test $status -eq 0
+        '/mnt/c/Program Files/Firefox Developer Edition/firefox.exe' $url
+    else
+        xdg-open $url
+    end
 end
