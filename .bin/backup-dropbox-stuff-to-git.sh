a79814f4972b4aa4f3f07eb06a7452038ec1c8a6
Chris Davison <c.jr.davison@gmail.com>
2019-12-18 09:50:44 +0000

    Fix typo in dropbox backup script

--- .bin/backup-dropbox-stuff-to-git.sh
+++ .bin/backup-dropbox-stuff-to-git.sh
@@ -8,26 +8,28 @@ backup_source_to_dest() {
     echo "Source: $src"
     echo "Destination: $dest"
     echo "Backup date: $(date +'%Y%m%d %H')"
-    pushd "$dest"
-    fd -E ".git/" -x rm -rf {}
-    rsync -r $source $dest
+    fd -E ".git" -x git rm -rf {}
+    rsync -r "$src" "$dest"
     git add --all
     git commit -m "backup $(date +'%Y%m%d %H')"
     git push
-    popd
 }
 
 notes() {
     src="$HOME/Dropbox/notes/"
     dest="$HOME/src/github.com/ChrisDavison/knowledge/"
+    cd "$dest"
     backup_source_to_dest "$src" "$dest"
 }
 
 logbook() {
     src="$HOME/Dropbox/logbook/"
     dest="$HOME/src/github.com/ChrisDavison/logbook/"
+    cd "$dest"
     backup_source_to_dest "$src" "$dest"
 }
 
+curdir=`pwd`
 notes
 logbook
+cd "$curdir"
