54f2527ff8e07312c46396a50ab32cf6044daf52
Chris Davison <c.jr.davison@gmail.com>
2020-01-07 13:51:06 +0000

    Join filename args when running newlit

--- .config/fish/functions/newlit.fish
+++ .config/fish/functions/newlit.fish
@@ -1,11 +1,11 @@
 function newlit
 	set url $argv[1]
-set target $argv[2]
-echo $url
-set stem (string split -r -m1 . $target)[1]
-echo "# $stem" > writeups/$stem.txt
-echo >> writeups/$stem.txt
-echo >> writeups/$stem.txt
-echo "@unread" >> writeups/$stem.txt
-curl $url --output papers/$target
+    set target (string join '-' $argv[2..(count $argv)])
+    echo $url
+    set stem (string split -r -m1 . $target)[1]
+    echo "# $stem" > writeups/$stem.txt
+    echo >> writeups/$stem.txt
+    echo >> writeups/$stem.txt
+    echo "@unread" >> writeups/$stem.txt
+    curl $url --output papers/$target.pdf
 end
