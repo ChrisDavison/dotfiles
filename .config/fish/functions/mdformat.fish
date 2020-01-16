6e09b86ee4ecc82e41dab6ac5e63fad5fe7cd53f
Chris Davison <c.jr.davison@gmail.com>
2020-01-16 15:10:08 +0000

    Don't use reference links in mdformat

--- .config/fish/functions/mdformat.fish
+++ .config/fish/functions/mdformat.fish
@@ -1,3 +1,4 @@
 function mdformat
-	pandoc --to markdown-shortcut_reference_links+pipe_tables-simple_tables --wrap=none --reference-links --reference-location=section --atx-headers $argv[1] -o $argv[1]
+	# pandoc --to markdown-shortcut_reference_links+pipe_tables-simple_tables --wrap=none --reference-links --reference-location=section --atx-headers $argv[1] -o $argv[1]
+	pandoc --to markdown+pipe_tables-simple_tables --wrap=none --atx-headers $argv[1] -o $argv[1]
 end
