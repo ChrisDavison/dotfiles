5b1e4607fa4be430120d436d4d2f5386244310b1
Chris Davison <c.jr.davison@gmail.com>
2020-02-07 10:20:34 +0000

    Make newlit prompt if not enough args given

--- .config/fish/functions/newlit.fish
+++ .config/fish/functions/newlit.fish
@@ -1,11 +1,19 @@
 function newlit
-	set url $argv[1]
-    set target (string join '-' $argv[2..(count $argv)])
-    echo $url
-    set stem (string split -r -m1 . $target)[1]
-    echo "# $stem" > writeups/$stem.txt
-    echo >> writeups/$stem.txt
-    echo >> writeups/$stem.txt
-    echo "@unread" >> writeups/$stem.txt
-    curl $url --output papers/$target.pdf
+    if test (count $argv) -eq 0
+        set url (string join "-" (string split " " (read -P "url: ")))
+        set filename (string join "-" (string split " " (read -P "filename: ")))
+    else if test (count $argv) -eq 1
+        set url $argv[1]
+        echo "One arg passed. Assuming it's the URL."
+        set filename (string join "-" (string split " " (read -P "filename: ")))
+    else
+        set url $argv[1]
+        set filename (string join '-' $argv[2..(count $argv)])
+    end
+    set ext (string split -r -m1 . $url)[2]
+    echo "# $filename" > $HOME/Dropbox/literature/writeups/$filename.txt
+    echo >> $HOME/Dropbox/literature/writeups/$filename.txt
+    echo >> $HOME/Dropbox/literature/writeups/$filename.txt
+    echo "@unread" >> $HOME/Dropbox/literature/writeups/$filename.txt
+    curl $url --output $HOME/Dropbox/literature/papers/$filename.$ext
 end
