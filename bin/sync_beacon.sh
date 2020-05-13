6308e89a74efc6317eb2fe08bc6ee350a24e21c9
Chris Davison <c.jr.davison@gmail.com>
2020-05-13 08:42:39 +0100

    sh: only sync beacon.csv
    i now run a cronjob on beacon to aggregate the csvs rather than
    transferring a bunch of small files.

--- bin/sync_beacon.sh
+++ bin/sync_beacon.sh
@@ -3,4 +3,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-pushd $HOME/Dropbox/work-data/beacon && rsync -rv beacon@192.168.0.27:data/2020_05 . && popd
+pushd $HOME/Dropbox/work-data/beacon && rsync -rv beacon@192.168.0.27:beacon.csv . && popd
