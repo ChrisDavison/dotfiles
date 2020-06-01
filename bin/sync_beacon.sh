b791549b580c7f23a78d166f0665db53ad38b18b
Chris Davison <c.jr.davison@gmail.com>
2020-06-01 16:21:34 +0100

    Update output path

--- bin/sync_beacon.sh
+++ bin/sync_beacon.sh
@@ -3,4 +3,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-pushd $HOME/Dropbox/work-data/beacon && rsync -rv beacon@192.168.0.27:beacon.csv . && popd
+pushd $HOME/Dropbox/work/work-data/beacon && rsync -rv beacon@81.98.224.48:beacon.csv . && popd
