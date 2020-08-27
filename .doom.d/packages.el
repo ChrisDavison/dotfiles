0a2f93d62c1fb70fe18c0013dae1d7aaebaffaf0
Chris Davison <c.jr.davison@gmail.com>
2020-08-27 15:28:34 +0100

    emacs: add packages 's' and 'f'
    utility 'string' and 'function' functions

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -23,6 +23,9 @@
 (package! pinboard)
 
 (package! yasnippet)
+
+(package! s)
+(package! f)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
