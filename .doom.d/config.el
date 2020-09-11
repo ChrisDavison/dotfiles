b8a0caf9b85eb974f92661b6cf558a35ea552ec0
Chris Davison <c.jr.davison@gmail.com>
2020-09-11 15:28:46 +0100

    emacs: golang config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -188,3 +188,17 @@
 ;; (org-roam-mode)
 
 (map! :leader "j" 'jump-to-register)
+
+(add-to-list 'exec-path (concat (file-name-as-directory (getenv "GOPATH")) "bin") t)
+(add-to-list 'load-path (concat (file-name-as-directory (getenv "GOPATH")) "src/github.com/dougm/goflymake"))
+(require 'go-flymake)
+; Use goimports instead of go-fmt for formatting with intelligent package addition/removal
+(setq gofmt-command "goimports")
+(add-hook 'go-mode-hook (lambda ()
+                          (set (make-local-variable 'company-backends) '(company-go))
+                          (local-set-key (kbd "M-.") 'godef-jump)
+                          (go-eldoc-setup)
+                          ; call Gofmt before saving
+                          (add-hook 'before-save-hook 'gofmt-before-save)))
+
+(map! "C-," 'avy-goto-char-2)
