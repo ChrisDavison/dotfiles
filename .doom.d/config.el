f0fdc1d19ae6d332c43b193697d0fd7ac8a0a225
Chris Davison <c.jr.davison@gmail.com>
2020-09-18 09:56:35 +0100

    org: change formatting of rust block

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -33,10 +33,11 @@
 
 ;;; Programming - rust
 (add-hook! rust-mode
-           '(company-mode flycheck-rust-setup
-                          cargo-minor-mode
-                          racer-mode
-                          (lambda () (add-to-list 'company-backends 'company-racer))))
+           '(company-mode
+             flycheck-rust-setup
+             cargo-minor-mode
+             racer-mode
+             (lambda () (add-to-list 'company-backends 'company-racer))))
 (add-hook! racer-mode '(company-mode eldoc-mode))
 (add-to-list 'auto-mode-alist '("\\.rs" . rust-mode))
 
