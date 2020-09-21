4282428b1985f755c559d50b4c6e6e7a903bbb7d
Chris Davison <c.jr.davison@gmail.com>
2020-09-21 23:16:28 +0100

    rofi: open all weather bookmarks

--- bin/rofi_bookmark_groups.sh
+++ bin/rofi_bookmark_groups.sh
@@ -15,7 +15,7 @@ else
     if [ ! -z "$option" ]; then
         case $(echo "$option" | cut -d' ' -f1) in
             EVERYDAY) firefox --new-tab "https://news.ycombinator.com" --new-tab "https://lobste.rs" --new-tab "https://reddit.com/r/programming"  --new-tab "https://reddit.com/r/rust" --new-tab "https://reddit.com/r/videos" --new-tab "https://www.bbc.co.uk/news" ;;
-            Weather) firefox --new-tab "https://darksky.net/forecast/55.8259,-4.2265/uk212/en" ;;
+            Weather) firefox --new-tab "https://darksky.net/forecast/55.8259,-4.2265/uk212/en" --new-tab "https://www.metoffice.gov.uk/weather/forecast/gcuvz3bch#?nearestTo=Glasgow%20(Glasgow%20City)" --new-tab "https://www.bbc.co.uk/weather/2648579" ;;
             Email) firefox --new-tab "https://outlook.office365.com/mail/inbox" --new-tab "https://mail.google.com" ;;
             Calendar) firefox --new-tab "https://outlook.office365.com/calendar/view/workweek" --new-tab "https://calendar.google.com" ;;
             Youtube) firefox --new-tab "https://www.youtube.com/playlist?list=WL" --new-tab "https://www.youtube.com/feed/subscriptions" --new-tab "https://www.youtube.com/";;
