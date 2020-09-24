b4c4a960bd6714001e36f96bc05248627c211164
Chris Davison <c.jr.davison@gmail.com>
2020-09-24 19:53:05 +0100

    remove asmr from rofi_bookmarks_group

--- bin/rofi_bookmark_groups.sh
+++ bin/rofi_bookmark_groups.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# browser=google-chrome
+browser=firefox
 
 menu="EVERYDAY
 Weather
 Email
 Calendar
 Youtube
-9gag
-ASMR"
+9gag"
 
 if [ -z "$@" ]; then
     cat <<< $menu  
@@ -14,13 +15,12 @@ else
     option=$1
     if [ ! -z "$option" ]; then
         case $(echo "$option" | cut -d' ' -f1) in
-            EVERYDAY) firefox --new-tab "https://news.ycombinator.com" --new-tab "https://lobste.rs" --new-tab "https://reddit.com/r/programming"  --new-tab "https://reddit.com/r/rust" --new-tab "https://reddit.com/r/videos" --new-tab "https://www.bbc.co.uk/news" ;;
-            Weather) firefox --new-tab "https://darksky.net/forecast/55.8259,-4.2265/uk212/en" --new-tab "https://www.metoffice.gov.uk/weather/forecast/gcuvz3bch#?nearestTo=Glasgow%20(Glasgow%20City)" --new-tab "https://www.bbc.co.uk/weather/2648579" ;;
-            Email) firefox --new-tab "https://outlook.office365.com/mail/inbox" --new-tab "https://mail.google.com" ;;
-            Calendar) firefox --new-tab "https://outlook.office365.com/calendar/view/workweek" --new-tab "https://calendar.google.com" ;;
-            Youtube) firefox --new-tab "https://www.youtube.com/playlist?list=WL" --new-tab "https://www.youtube.com/feed/subscriptions" --new-tab "https://www.youtube.com/";;
-            9gag) firefox --new-tab "https://www.9gag.com" --new-tab "https://www.9gag.com/girl" --new-tab "https://www.9gag.com/nsfw";;
-            ASMR) firefox --new-tab "$(randomasmr)" ;;
+            EVERYDAY) $browser --new-tab "https://news.ycombinator.com" --new-tab "https://lobste.rs" --new-tab "https://reddit.com/r/programming"  --new-tab "https://reddit.com/r/rust" --new-tab "https://reddit.com/r/videos" --new-tab "https://www.bbc.com/news/uk" ;;
+            Weather) $browser --new-tab "https://darksky.net/forecast/55.8259,-4.2265/uk212/en" ;;
+            Email) $browser --new-tab "https://outlook.office365.com/mail/inbox" --new-tab "https://mail.google.com" ;;
+            Calendar) $browser --new-tab "https://outlook.office365.com/calendar/view/workweek" --new-tab "https://calendar.google.com" ;;
+            Youtube) $browser --new-tab "https://www.youtube.com/playlist?list=WL" --new-tab "https://www.youtube.com/feed/subscriptions" --new-tab "https://www.youtube.com/";;
+            9gag) $browser --new-tab "https://www.9gag.com" --new-tab "https://www.9gag.com/girl" --new-tab "https://www.9gag.com/nsfw";;
             *) exit 1 ;;
         esac
     fi
