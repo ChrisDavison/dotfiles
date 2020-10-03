aaf5dad9444f8e44e60d5c8ce8feba96f721c723
Chris Davison <c.jr.davison@gmail.com>
2020-10-03 08:36:58 +0100

    xmonad: go back to plain keybind definitions

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,7 @@
 import Control.Arrow (first)
 import Data.List (isInfixOf)
 import System.Exit
-
+import Graphics.X11.ExtraTypes.XF86
 import XMonad
 import XMonad.Actions.CycleWS (toggleWS', moveTo, findWorkspace, shiftTo, WSType( HiddenNonEmptyWS, EmptyWS ))
 import XMonad.Actions.Submap (submap)
@@ -13,7 +13,6 @@ import XMonad.Layout.CenteredMaster (centerMaster)
 import XMonad.Layout.NoBorders (noBorders)
 import XMonad.Layout.Spacing (spacing)
 import XMonad.Layout.ToggleLayouts
-import XMonad.Layout.TwoPanePersistent
 import XMonad.Prompt
 import XMonad.Prompt.FuzzyMatch
 import XMonad.Util.Paste
@@ -71,6 +70,11 @@ gotoNonEmptyWS dir = windows . W.view =<< ws
   where
     ws = findWorkspace getSortByIndex dir HiddenNonEmptyWS 1
 
+shiftToNonEmptyWS :: Direction1D -> X()
+shiftToNonEmptyWS dir = windows . W.shift =<< ws
+  where
+    ws = findWorkspace getSortByIndex dir EmptyWS 1
+
 raiseEmacsAndRun :: String -> X()
 raiseEmacsAndRun cmd = sequence_ [raise (className =? "Emacs"), runEmacs cmd]
 
@@ -88,135 +92,137 @@ q =?? f = fmap (f `isInfixOf`) q
 q =?! f = fmap (fuzzyMatch f) q
 
 -- KEYBINDING
--- Key aliases, as a reminder for keybinds (mod2 is numlock)
-keyAlt = mod1Mask
-keyHyper = mod3Mask
-keyWindows = mod4Mask
-myModMask = keyHyper
+-- 
+-- mod1Mask == alt
+-- mod2Mask == num lock
+-- mod3Mask == hyper (i've rebound to caps, but have some problems)
+-- mod4Mask == windows
+myModMask :: KeyMask
+myModMask = mod4Mask
+
 -- AVAILABLE - F5..F12  (and shift-F1..F12)
 --             `~ 0 S-0 -_ =+ Insert
 --             tabTAB W eE R T yY uU iI oO \
 --             D F gG H ;: '"
 --             zZ xX c vV B nN mM <> /?
 --             [_,S-][left, right, up, down]
---
-myMouseBindings conf = M.fromList [
-      withHyper button1 (\w -> focus w >> mouseMoveWindow w) -- Left = move
-    , withHyper button2 (\w -> focus w >> windows W.swapMaster) -- Middle = make master
-    , withHyper button3 (\w -> focus w >> mouseResizeWindow w) -- Right = resize
+myMouseBindings :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X())
+myMouseBindings _conf = M.fromList [
+      ((mod4Mask, button1), (\w -> focus w >> mouseMoveWindow w)) -- Left = move
+    , ((mod4Mask, button2), (\w -> focus w >> windows W.swapMaster)) -- Middle = make master
+    , ((mod4Mask, button3), (\w -> focus w >> mouseResizeWindow w)) -- Right = resize
     -- Mouse wheel is sensitive, so use  smaller volume increments
-    , withHyper button4 (\w -> spawn "amixer set Master 3dB+") -- Scroll up = vol up
-    , withHyper button5 (\w -> spawn "amixer set Master 3dB-")] -- Scroll down = vol down
-  where
-    keybind keymask key command = ((keymask, key), command)
-    withHyper key command = keybind mod3Mask key command
+    , ((mod4Mask, button4), (\w -> (spawn "amixer sset Master 3dB+"))) -- Scroll up = vol up
+    , ((mod4Mask, button5), (\w -> (spawn "amixer sset Master 3dB-")))] -- Scroll down = vol down
 
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X())
 myKeys conf = M.fromList $ [
-      withHyperShift xK_q       (io (exitWith ExitSuccess)) -- QUIT
-    , withHyperShift xK_c       (kill) -- close the focused window
-    , withHyper      xK_q       (restart "xmonad" True)
+      ((mod4Mask .|. shiftMask, xK_q),       (io (exitWith ExitSuccess))) -- QUIT
+    , ((mod4Mask .|. shiftMask, xK_c),       kill) -- close the focused window
+    , ((mod4Mask,               xK_q),       restart "xmonad" True)
     -- Changing window focus
-    , withHyper      xK_bracketleft   (gotoNonEmptyWS Prev) 
-    , withHyper      xK_bracketright  (gotoNonEmptyWS Next)
-    , withHyper      xK_j       (windows W.focusDown) -- focus window up stack
-    , withHyper      xK_k       (windows W.focusUp  ) -- focus window down stack
-    , withHyper      xK_m       (windows W.focusMaster  ) -- focus master window
-    , withHyperShift xK_j       (windows W.swapDown) -- move window up stack
-    , withHyperShift xK_k       (windows W.swapUp  ) -- move window down stack
-    , withHyperShift xK_m       (windows W.swapMaster) -- make window stack master
-    , withHyper      xK_h       (sendMessage Shrink) -- fewer windows in master
-    , withHyper      xK_l       (sendMessage Expand) -- more windows in master
+    , ((mod4Mask,               xK_bracketleft), gotoNonEmptyWS Prev) 
+    , ((mod4Mask,               xK_bracketright), gotoNonEmptyWS Next)
+    , ((mod4Mask .|. shiftMask, xK_bracketleft),   shiftToNonEmptyWS Prev) 
+    , ((mod4Mask .|. shiftMask, xK_bracketright),  shiftToNonEmptyWS Next)
+    , ((mod4Mask,               xK_j), windows W.focusDown) -- focus window up stack
+    , ((mod4Mask,               xK_k), windows W.focusUp  ) -- focus window down stack
+    , ((mod4Mask,               xK_m), windows W.focusMaster  ) -- focus master window
+    , ((mod4Mask .|. shiftMask, xK_j), windows W.swapDown) -- move window up stack
+    , ((mod4Mask .|. shiftMask, xK_k), windows W.swapUp  ) -- move window down stack
+    , ((mod4Mask .|. shiftMask, xK_m), windows W.swapMaster) -- make window stack master
+    , ((mod4Mask,               xK_h), sendMessage Shrink) -- fewer windows in master
+    , ((mod4Mask,               xK_l), sendMessage Expand) -- more windows in master
     -- Changing layout
-    , withHyper      xK_space   (sendMessage NextLayout) -- Use next configured layout
-    , withHyperShift xK_space   (setLayout $ XMonad.layoutHook conf) -- reset to default layout
-    , withHyper      xK_f       (fsNoBar) -- toggle fullscreen on focused window
-    , withHyperShift xK_f       (sendMessage ToggleStruts) -- toggle fullscreen on focused window
+    , ((mod4Mask,               xK_space), sendMessage NextLayout) -- Use next configured layout
+    , ((mod4Mask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf) -- reset to default layout
+    , ((mod4Mask,               xK_f), fsNoBar) -- toggle fullscreen on focused window
+    , ((mod4Mask .|. shiftMask, xK_f), sendMessage ToggleStruts) -- toggle fullscreen on focused window
     -- dumb function to turn my external monitor 'off'
     -- by just displaying a fullscreen black wallpaper
-    , withHyper      xK_o       (spawn "feh $HOME/Dropbox/pictures/wallpapers/justblack.png"
+    , ((mod4Mask,               xK_o), (spawn "feh $HOME/Dropbox/pictures/wallpapers/justblack.png"
                                  <+> (flip whenJust (windows . W.view) =<< screenWorkspace 1)
-                                 <+> fsNoBar) -- toggle fullscreen on focused window
-    , withHyperShift xK_o       (raiseAndDo (return ()) (title =?? "justblack.png") (\w -> kill))
-    , withHyper      xK_t       (withFocused $ windows . W.sink) -- make float tiled again , withHyper      xK_comma   (sendMessage (IncMasterN 1)) -- increase num master windows
-    , withHyper      xK_period  (sendMessage (IncMasterN (-1))) -- decrease num master windows
+                                 <+> fsNoBar)) -- toggle fullscreen on focused window
+    , ((mod4Mask .|. shiftMask, xK_o), raiseAndDo (return ()) (title =?? "justblack.png") (\w -> kill))
+    , ((mod4Mask,               xK_t), withFocused $ windows . W.sink) -- make float tiled again , withWin      xK_comma   (sendMessage (IncMasterN 1)) -- increase num master windows
+    , ((mod4Mask,               xK_period), sendMessage (IncMasterN (-1))) -- decrease num master windows
     -- Launchers
-    , withHyper      xK_Return  (spawn myTerminal)
-    , withHyper      xK_p       (spawn myJ4Command)
-    , withHyper      xK_w       (spawn "dmenu_win_switcher.sh")
-    , withHyper      xK_b       (spawn "dmenu_bookmark_groups.sh")
-    , withHyperShift xK_b       (spawn "dmenu_bookmarks.sh")
-    , withHyper      xK_r       (spawn $ myTerminal ++ " -e ranger")
-    , withHyper      xK_x       (spawn $ myTerminal ++ " -t scratchpad -e $HOME/.bin/get_xprop.sh")
+    , ((mod4Mask,               xK_Return), spawn myTerminal)
+    , ((mod4Mask,               xK_p), spawn myJ4Command)
+    , ((mod4Mask,               xK_w), spawn "dmenu_win_switcher.sh")
+    , ((mod4Mask,               xK_b), spawn "dmenu_bookmark_groups.sh")
+    , ((mod4Mask .|. shiftMask, xK_b), spawn "dmenu_bookmarks.sh")
+    , ((mod4Mask,               xK_r), spawn $ myTerminal ++ " -e ranger")
+    , ((mod4Mask,               xK_x), spawn $ myTerminal ++ " -t scratchpad -e $HOME/.bin/get_xprop.sh")
     -- Emacs / org mode
-    , withHyper      xK_F1      (raiseEmacsAndRun "(org-capture)")
-    , withHyper      xK_F2      (raiseEmacsAndRun "(org-agenda)")
-    , withHyper      xK_F3      (raiseEmacsAndRun "(org-agenda nil \"c1\")")
-    , withHyper      xK_F4      (raiseEmacsAndRun "(org-agenda nil \"cW\")")
+    , ((mod4Mask,               xK_F1), raiseEmacsAndRun "(org-capture)")
+    , ((mod4Mask,               xK_F2), raiseEmacsAndRun "(org-agenda)")
+    , ((mod4Mask,               xK_F3), raiseEmacsAndRun "(org-agenda nil \"c1\")")
+    , ((mod4Mask,               xK_F4), raiseEmacsAndRun "(org-agenda nil \"cW\")")
     -- Other apps
-    , withHyper      xK_F5      (runOrRaise "spotify" (className =? "Spotify"))
-    , withHyper      xK_F6      (raiseNextMaybe (spawn "firefox") (className =? "Firefox"))
-    , withHyper      xK_F7      (raiseNext (title =?? "ASMR"))
-    , withHyper      xK_F11     (spawn "dmenu_asmr.py")
-    , withHyper      xK_F12     (S.promptSearch myXPConfig S.duckduckgo)
+    , ((mod4Mask,               xK_F5), runOrRaise "spotify" (className =? "Spotify"))
+    , ((mod4Mask,               xK_F6), raiseNextMaybe (spawn "firefox") (className =? "Firefox"))
+    , ((mod4Mask,               xK_F7), raiseNext (title =?? "ASMR"))
+    , ((mod4Mask,               xK_F11), spawn "dmenu_asmr.py")
+    , ((mod4Mask,               xK_F12), S.promptSearch myXPConfig S.duckduckgo)
     -- Media keys
-    , withHyper      xK_Home    (spawn "amixer set Master 6dB+")
-    , withHyper      xK_End     (spawn "amixer set Master 6dB-")
-    , withHyper      xK_Delete  (spawn "$HOME/.bin/spotify.sh prev")
-    , withHyper      xK_Next    (spawn "$HOME/.bin/spotify.sh next")
-    , withHyper      xK_Prior   (spawn "$HOME/.bin/spotify.sh play-pause")
+    , ((mod4Mask,               xK_Home), spawn "amixer sset Master 6dB+")
+    , ((mod4Mask,               xK_End), spawn "amixer sset Master 6dB-")
+    , ((mod4Mask,               xK_Delete), spawn "$HOME/.bin/spotify.sh prev")
+    , ((mod4Mask,               xK_Next), spawn "$HOME/.bin/spotify.sh next")
+    , ((mod4Mask,               xK_Prior), spawn "$HOME/.bin/spotify.sh play-pause")
+    , ((0,                      xF86XK_AudioRaiseVolume), spawn "amixer set Master 6dB+")
+    , ((0,                      xF86XK_AudioLowerVolume), spawn "amixer set Master 6dB-")
+    , ((0,                      xF86XK_AudioPrev), spawn "$HOME/.bin/spotify.sh prev")
+    , ((0,                      xF86XK_AudioNext), spawn "$HOME/.bin/spotify.sh next")
+    , ((0,                      xF86XK_AudioPlay), spawn "$HOME/.bin/spotify.sh play-pause")
+    -- Brightness
+    , ((0,                      xF86XK_KbdBrightnessUp), spawn "bright.sh up")
+    , ((0,                      xF86XK_MonBrightnessUp), spawn "bright.sh up")
+    , ((0,                      xF86XK_KbdBrightnessDown), spawn "bright.sh down")
+    , ((0,                      xF86XK_MonBrightnessDown), spawn "bright.sh down")
     -- KEYCHORD - H-d {e,a} -- open ebooks or literature
-    , withHyper      xK_d       (submap . M.fromList $ [
-                      (only xK_e (spawn "dmenu_ebooks.sh"))
-                    , (only xK_a (spawn "dmenu_articles.sh"))])
+    , ((mod4Mask,               xK_d),  submap . M.fromList $ [
+                           (((0, xK_e), spawn "dmenu_ebooks.sh"))
+                         , (((0, xK_a), spawn "dmenu_articles.sh"))])
     ]
 
-    -- mod-[1..9] view workspace on current monitor
+    -- -- mod-[1..9] view workspace on current monitor
     -- win-[1..9] view workspace on main monitor 
     -- alt-[1..9] view workspace on second monitor
     -- mod-shift-[1..9] move window to workspace 
-    ++ [withHyper key (windows $ W.greedyView ws)                    
+    ++ [((mod3Mask, key), windows $ W.greedyView ws)                    
         | (key, ws) <- zip [xK_1..xK_9] myWorkspaces]
-    ++ [withWin key (sequence_ [
+    ++ [((mod4Mask, key), (sequence_ [
                              flip whenJust (windows . W.view) =<< screenWorkspace 0,
-                             windows $ W.greedyView ws])                
+                             windows $ W.greedyView ws]))                
         | (key, ws) <- zip [xK_1..xK_9] myWorkspaces]
-    ++ [withAlt key (sequence_ [
+    ++ [((mod1Mask, key), (sequence_ [
                              flip whenJust (windows . W.view) =<< screenWorkspace 1,
-                             windows $ W.greedyView ws])       
+                             windows $ W.greedyView ws]))      
         | (key, ws) <- zip [xK_1..xK_9] myWorkspaces]
-    ++ [withHyperShift key (windows $ W.shift ws)                      
+    ++ [((mod4Mask .|. shiftMask, key), windows $ W.shift ws)                      
         | (key, ws) <- zip [xK_1..xK_9] myWorkspaces]
     -- mod-{a,s} focus physical screen
     -- mod-shift-{a,s} move window physical screen
-    ++ [withHyper key (flip whenJust (windows . W.view) =<< screenWorkspace sc)
+    ++ [((mod4Mask, key), flip whenJust (windows . W.view) =<< screenWorkspace sc)
         | (key, sc) <- zip [xK_a, xK_s] [0..]]
     -- This is equivalent to /shift focused window to whatever workspace is on monitor N/
     -- Do i really need this keybind? I have relatively set keybinds
-    ++ [withHyperShift key (flip whenJust (windows . W.shift) =<< screenWorkspace sc)
+    ++ [((mod4Mask .|. shiftMask, key), flip whenJust (windows . W.shift) =<< screenWorkspace sc)
         | (key, sc) <- zip [xK_a, xK_s] [0..]]
     where
-      keybind mask   key command = ((mask, key), command)
-      withHyper      key command = keybind mod3Mask key command
-      withHyperShift key command = keybind (mod3Mask .|. shiftMask) key command
-      withHyperAlt   key command = keybind (mod3Mask .|. keyAlt) key command
-      withHyperWin   key command = keybind (mod3Mask .|. keyWindows) key command
-      withAlt        key command = keybind keyAlt key command
-      withAltShift   key command = keybind (keyAlt .|. shiftMask) key command
-      withWin        key command = keybind keyWindows key command
-      withWinShift   key command = keybind keyWindows key command
-      only key command = keybind 0 key command
       fsNoBar                    = sendMessage (Toggle "Full") <+> sendMessage ToggleStruts
 
 ------------------------------------------------------------------------
 -- Layouts:
-myLayout = twoThirdsMasterTiled ||| tiled ||| twoPane
+myLayout = twoThirdsMasterTiled ||| tiled
   where
      -- Layouts
      cleanFull = noBorders Full
      centerOverTile = toggleLayouts cleanFull $ centerMaster tiled
      twoThirdsMasterTiled = toggleLayouts cleanFull $ spacing 10 $ Tall nmaster delta ratio2
      tiled   = toggleLayouts cleanFull $ spacing 10 $ Tall nmaster delta ratio
-     twoPane = toggleLayouts cleanFull $ spacing 10 $ TwoPanePersistent Nothing delta ratio
      -- Config
      nmaster = 1 -- Number of windows in master pane
      ratio2 = 2/3 -- Use 2/3 of screen for master
@@ -261,7 +267,7 @@ myConfig pipe = defaultConfig {
     terminal           = myTerminal,
     focusFollowsMouse  = True,
     borderWidth        = 3,
-    modMask            = mod3Mask,
+    modMask            = myModMask,
     workspaces         = myWorkspaces,
     normalBorderColor  = myColourGrey,
     focusedBorderColor = myColourPurple,
