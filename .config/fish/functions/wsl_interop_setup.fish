7568a0e7dee17c86ab5f2365d7096a1c33fcb492
Chris Davison <c.jr.davison@gmail.com>
2020-11-09 19:55:34 +0000

    Don't echo WSL interop as it breaks emacs' support

--- .config/fish/functions/wsl_interop_setup.fish
+++ .config/fish/functions/wsl_interop_setup.fish
@@ -5,12 +5,13 @@ function wsl_interop_setup
     set -Ux LIBGL_ALWAYS_INDIRECT 1
 
     set -Ux NO_AT_BRIDGE 1
+    rm ~/.wsl_interop
     for i in (pstree -np -s $fish_pid | grep -o -E '[0-9]+')
         set -l fname "/run/WSL/"$i"_interop"
         if test -e $fname
             set -x WSL_INTEROP $fname
             echo $fname >~/.wsl_interop
-            echo "WSL Interop "$WSL_INTEROP
+            # echo "WSL Interop "$WSL_INTEROP
         end
     end
 end
