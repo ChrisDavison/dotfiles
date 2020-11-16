1a0f8ff54ceb96b87231f41b3ea65004301dd7da
Chris Davison <c.jr.davison@gmail.com>
2020-11-16 14:01:05 +0000

    emacs: remove a bunch of unused packages

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -23,29 +23,15 @@
 (package! fish-mode)
 (package! nov)
 (package! graphviz-dot-mode)
-(package! csharp-mode)
-(package! fsharp-mode)
 (package! haskell-mode)
 (package! yaml-mode)
 (package! ahk-mode)
 
 ;;; Notetaking
-;; (package! zetteldeft)
-(package! org-roam-server)
 (package! doct)  ; org-capture, decleratively
 (package! anki-editor)
-(package! org-super-agenda)
-(package! org-superstar)
 
 ;;; Themes
-(package! minimal-theme)
-(package! tao-theme)
 (package! apropospriate-theme)
 (package! dracula-theme)
-(package! flatland-theme)
-(package! spacemacs-theme)
-(package! color-theme-sanityinc-tomorrow)
-(package! afternoon-theme)
 (package! kaolin-themes)
-(package! cyberpunk-theme)
-(package! seoul256-theme)
