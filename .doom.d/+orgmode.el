f5dae55583c6042d779e1390a13f914c4ac76948
Chris Davison <c.jr.davison@gmail.com>
2020-11-21 17:38:04 +0000

    org: save all org buffers with autosave

--- .doom.d/+orgmode.el
+++ .doom.d/+orgmode.el
@@ -1,6 +1,13 @@
-;;; ../code/dotfiles/.doom.d/autoload/orgutil.el -*- lexical-binding: t; -*-
+;; ../code/dotfiles/.doom.d/autoload/orgutil.el -*- lexical-binding: t; -*-
 
 ;;; general org settings
+;;;
+(defun get-agenda-files (to-ignore)
+  (let* ((ignores (s-join "|" to-ignore))
+         (agenda-dir (expand-file-name "~/Dropbox/org/projects"))
+         (files (directory-files-recursively agenda-dir "\.org$")))
+    (--remove (s-matches? (format "(%s)$" ignores) it) files)))
+
 (setq org-directory (expand-file-name "~/Dropbox/org")
       org-default-notes-file (expand-file-name "~/Dropbox/org/inbox.org")
       org-src-window-setup 'current-window
@@ -29,7 +36,7 @@
       org-archive-location "~/Dropbox/org/projects/done.org::* From %s"
       org-refile-use-outline-path 't
       org-refile-allow-creating-parent-nodes 'confirm
-      org-refile-targets '((org-agenda-files . (:maxlevel . 3)))
+      org-refile-targets `((org-agenda-files . (:maxlevel . 3)))
       org-roam-directory org-directory
       org-startup-folded 'fold
       org-id-track-globally t
@@ -169,11 +176,12 @@
                              "......"
                              "")
       org-agenda-skip-archived-trees nil
+      org-image-actual-width 600
       org-agenda-use-time-grid nil
       org-overriding-columns-format "%TODO %3PRIORITY %DEADLINE %40ITEM %TAGS"
       org-agenda-sorting-strategy
       '((agenda habit-up category-up scheduled-up  time-up todo-state-up  priority-down)
-        (todo todo-state-down priority-down category-keep)
+        (todo category-up todo-state-up priority-down)
         (tags priority-down category-keep)
         (search category-keep)))
 
@@ -196,14 +204,11 @@
                       (org-agenda-start-day "-0d")
                       (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("BLCK" "WAIT")))))
           (todo "WIP" ((org-agenda-overriding-header "In Progress ('open loops')")
-                       (org-agenda-todo-ignore-scheduled t)
-                       (org-agenda-tag-filter-preset '("-readinglist" "-hobby"))))
+                       (org-agenda-todo-ignore-scheduled t)))
           (todo "WAIT|BLCK" ((org-agenda-overriding-header "Blocked or on-hold ('open loops')")
-                       (org-agenda-todo-ignore-scheduled t)
-                       (org-agenda-tag-filter-preset '("-readinglist" "-hobby"))))
+                             (org-agenda-todo-ignore-scheduled t)))
           (todo "NEXT" ((org-agenda-overriding-header "Possible next tasks")
-                        (org-agenda-todo-ignore-scheduled t)
-                        (org-agenda-tag-filter-preset '("-readinglist" "-hobby"))))))
+                        (org-agenda-todo-ignore-scheduled t)))))
 
         ("cn" "NEXT" ((todo "NEXT" nil)))
 
@@ -212,27 +217,24 @@
                              (org-agenda-overriding-header "Work")))))
 
         ("ct" "Todos, no books"
-         ((todo "" ((org-agenda-tag-filter-preset
-                     '("-readinglist" "-hobby"))))))
+         ((todo "" ((org-agenda-tag-filter-preset '("-readinglist"))))))
 
-        ("cr" "Review (Last 7 days' work)"
+        ("cr" "Review the last week"
          ((agenda "" ((org-super-agenda-groups nil)
                       (org-agenda-span 7)
                       (org-agenda-start-day "-8d")
                       (org-agenda-entry-types '(:timestamp))
                       (org-agenda-show-log t)))))
 
-        ("cp" "Planning (Work and Personal TODO)"
-         ((todo "WIP" ((org-agenda-files '("~/Dropbox/org/projects/work.org"
-                              "~/Dropbox/org/projects/thesis.org"))
-                       (org-agenda-overriding-header "Work IN PROGRESS")))
-          (todo "TODO" ((org-agenda-files '("~/Dropbox/org/projects/work.org"
-                              "~/Dropbox/org/projects/thesis.org")))
-                (org-agenda-overriding-header "Work TODO"))
-          (todo "WIP" ((org-agenda-files '("~/Dropbox/org/projects/todo.org"))
-                       (org-agenda-overriding-header "Personal IN PROGRESS")))
-          (todo "TODO" ((org-agenda-files '("~/Dropbox/org/projects/todo.org"))
-                        (org-agenda-overriding-header "Personal TODO")))))
+        ("cp" "Planning"
+         ((todo "WIP" ((org-agenda-overriding-header "IN PROGRESS")
+                       (org-agenda-tag-filter-preset '("-readinglist"))))
+          (todo "NEXT" ((org-agenda-overriding-header "POSSIBLE NEXT TASKS")
+                        (org-agenda-tag-filter-preset '("-readinglist"))))
+          (todo "TODO" ((org-agenda-overriding-header "TODO")
+                        (org-agenda-tag-filter-preset '("-readinglist"))))
+          (todo "MAYB" ((org-agenda-overriding-header "UNSURE ABOUT")
+                        (org-agenda-tag-filter-preset '("-readinglist"))))))
 
         ("cR" "Reading (books in progress, and next options)"
          ((todo "WIP"
@@ -240,7 +242,13 @@
                  (org-agenda-overriding-header "Books in Progress")))
           (todo "NEXT"
                 ((org-agenda-files '("~/Dropbox/org/projects/reading.org"))
-                 (org-agenda-overriding-header "Possible next books")))))
+                 (org-agenda-overriding-header "Possible next books")))
+          (todo "MAYB"
+                ((org-agenda-files '("~/Dropbox/org/projects/reading.org"))
+                 (org-agenda-overriding-header "Books to consider")))
+          (todo "TODO"
+                ((org-agenda-files '("~/Dropbox/org/projects/reading.org"))
+                 (org-agenda-overriding-header "Rest of the books...")))))
 
         ("cL" "Literature in progress, and next options"
          ((todo "WIP"
@@ -256,3 +264,5 @@
       org-journal-date-format "%F %A"
       org-journal-time-format ""
       org-journal-dir (f-join org-directory "projects")))
+
+(setq auto-save-hook 'org-save-all-org-buffers)
