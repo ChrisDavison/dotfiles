57b2fc18c4b5c4f4805fcafc39fa1800c5ae9b9a
Chris Davison <c.jr.davison@gmail.com>
2020-11-29 23:06:09 +0000

    Move literature capture into standalone file
    May want it in future, but it was just sitting commented out.

---
+++ .doom.d/+literature_capture.el
@@ -0,0 +1,41 @@
+;;; ../code/dotfiles/.doom.d/+literature_capture.el -*- lexical-binding: t; -*-
+
+;;; org-capture for literature
+(defun read-capitalized-title ()
+  (s-titleize (read-string "Title: ")))
+
+(defun read-author ()
+  (let ((name (read-string "Author: " "" nil nil)))
+    (if (s-equals? name "")
+        nil
+      (format-author-name name))))
+
+(defun format-author-name (author)
+  (concat (seq-mapcat
+           (lambda (author-part)
+             (if (> (length author-part) 1)
+                 (s-concat " " (s-capitalize author-part))
+               (s-concat (s-capitalize author-part) ".")))
+           (s-split " " author))))
+
+(defun maybe-get-bibtex ()
+  "Maybe get a DOI number for a reference"
+  (let ((doi (read-string "DOI: " "" nil nil)))
+    (if (s-equals? doi "")
+        nil
+      (s-concat ("\n")))))
+
+(defun read-authors ()
+  (setq authors (read-author)
+        running t)
+  (while running
+    (setq input (read-author))
+    (if (s-equals? input nil)
+        (setq running nil)
+      (setq authors (concat authors " and " input))))
+  authors)
+
+(setq capture-template-literature
+      `("L" ,(emoji-heading 'all-the-icons-faicon "graduation-cap" "Literature")
+        entry (file+headline "literature.org" "REFILE")
+        "* TODO %(read-capitalized-title)\n\nAuthors: %(read-authors)\n\n#+BEGIN_SRC bibtex\n#+END_SRC"))
