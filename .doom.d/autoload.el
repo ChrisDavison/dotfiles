94da91bc479377a0ff0903f2184e5e0bf1ad03e1
Chris Davison <c.jr.davison@gmail.com>
2020-11-29 23:07:07 +0000

    emacs: functions for unfilling paragraphs

--- .doom.d/autoload.el
+++ .doom.d/autoload.el
@@ -323,3 +323,23 @@ exist after each headings's drawers."
   (interactive)
   (goto-char (point-min))
   (search-forward "<<here>>"))
+
+;;;###autoload
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+;;;###autoload
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun org-unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (org-fill-paragraph nil region)))
