31320d2ee790942411b002608c04f0ba74808aff
Chris Davison <c.jr.davison@gmail.com>
2021-03-03 09:48:18 +0000

    Silently rm previous wsl_interop file

--- .config/fish/functions/wsl_interop_setup.fish
+++ .config/fish/functions/wsl_interop_setup.fish
@@ -5,13 +5,12 @@ function wsl_interop_setup
     set -Ux LIBGL_ALWAYS_INDIRECT 1
 
     set -Ux NO_AT_BRIDGE 1
-    test -f ~/.wsl_interop; and rm ~/.wsl_interop
+    rm -f ~/.wsl_interop
     for i in (pstree -np -s $fish_pid | grep -o -E '[0-9]+')
         set -l fname "/run/WSL/"$i"_interop"
         if test -e $fname
             set -x WSL_INTEROP $fname
-            echo $fname >~/.wsl_interop
-            # echo "WSL Interop "$WSL_INTEROP
+            echo $fname > ~/.wsl_interop
         end
     end
 end
